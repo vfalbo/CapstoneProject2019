a3a9f5db1083d062a138228993947f98e1edbe5d
NauseousHustler882 <59061949+NauseousHustler882@users.noreply.github.com>
2020-01-10 12:07:49 -0500

    Add files via upload

---
+++ thirdpersonchar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersonchar : MonoBehaviour
+{
+    public float speed;
+    private float rspeed;
+    private double dspeed;
+    private float currentspeed;
+    
+    
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {     
+
+        float hor = -(Input.GetAxis("Horizontal"));
+        float ver = -(Input.GetAxis("Vertical"));
+
+        
+
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * rspeed * Time.deltaTime;
+     
+        transform.Translate(playerMovement, Space.Self);
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            rspeed = speed + 5;
+        }
+        else
+        {
+            rspeed = speed;
+        }
+
+    }
+}
